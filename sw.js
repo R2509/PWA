6469c3aaa3c0fb14ce765a165f6205e34e9c588d
R2509 <101217029+R2509@users.noreply.github.com>
2022-09-16 16:00:13 +0000

    add service worker

---
+++ sw.js
@@ -0,0 +1,16 @@
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+      caches.open('fox-store').then((cache) => cache.addAll([
+        '/PWA/',
+        '/PWA/app.js',
+        '/PWA/index.html',
+      ])),
+    );
+  });
+  
+  self.addEventListener('fetch', (e) => {
+    console.log(e.request.url);
+    e.respondWith(
+      caches.match(e.request).then((response) => response || fetch(e.request)),
+    );
+  });
