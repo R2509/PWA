afa70708730a7521d5c98d9af6d81ece8b223130
R2509 <rich.seychelles@hotmail.com>
2022-11-09 19:24:07 +0000

    fbyufre

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-      .register('/PWA/sw.js')
-      .then(() => {console.log('Service Worker Registered')})
-      .catch((error) => {console.log(`Error: ${error}`)});
+		navigator.serviceWorker
+			.register('/PWA/sw.js')
+			.then(() => {console.log('Service Worker Registered')})
+			.catch((error) => {console.log(`Error: ${error}`)});
 }
+
+
+const button = document.getElementById('notifications');
+button.addEventListener('click', () => {
+  Notification.requestPermission().then((result) => {
+    if (result === 'granted') {
+      randomNotification();
+    }
+  });
+})
+
+function randomNotification() {
+	const randomItem = Math.floor(Math.random() * games.length);
+	const notifTitle = games[randomItem].name;
+	const notifBody = `Created by ${games[randomItem].author}.`;
+	const notifImg = `data/img/${games[randomItem].slug}.jpg`;
+	const options = {
+	  body: notifBody,
+	  icon: notifImg,
+	};
+	new Notification(notifTitle, options);
+	setTimeout(randomNotification, 30000);
+  }
--- sw.js
+++ sw.js
@@ -8,26 +8,27 @@ let cachefiles = [
 
 self.addEventListener('install', (e) => {
 	console.log('[Service Worker]: install');
-		e.waitUntil((
-			async () => {
-				const cache = await caches.open(cacheName);
-				console.log('[Service Worker]: caching app shell and content');
-				await cache.addAll(cachefiles);
-			}
-		)());
-	});
-	
-	self.addEventListener('fetch', (e) => {
-		e.respondWith((
-			async () => {
-				const r = await caches.match(e.request);
-				console.log(`[Service Worker]: resource: fetching ${e.request.url}`);
-				if (r) { return r; }
-				const response = await fetch(e.request);
-				const cache = await caches.open(cacheName);
-				console.log(`[Service Worker]: resource: caching ${e.request.url}`);
-				cache.put(e.request, response.clone());
-				return response;
-			}
-		)());
-	});
+	e.waitUntil((
+		async () => {
+			const cache = await caches.open(cacheName);
+			console.log('[Service Worker]: caching app shell and content');
+			await cache.addAll(cachefiles);
+		}
+	)());
+});
+
+self.addEventListener('fetch', (e) => {
+	e.respondWith((
+		async () => {
+			const r = await caches.match(e.request);
+			console.log(`[Service Worker]: resource: fetching ${e.request.url}`);
+			if (r) { return r; }
+			const response = await fetch(e.request);
+			const cache = await caches.open(cacheName);
+			console.log(`[Service Worker]: resource: caching ${e.request.url}`);
+			cache.put(e.request, response.clone());
+			return response;
+		}
+	)());
+});
+
