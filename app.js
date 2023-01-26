262e6ffb7d47bd5ad8d85408515ec822a76676ce
R2509 <rich.seychelles@hotmail.com>
2023-01-26 00:49:29 +0000

    bruh

--- app.js
+++ app.js
@@ -21,8 +21,9 @@ function randomNotification() {
 	const notifBody = `a notification :)`;//`Created by ${games[randomItem].author}.`;
 	//const notifImg = `data/img/${games[randomItem].slug}.jpg`;
 	const options = {
-	  body: notifBody,
-	//  icon: notifImg,
+		body: notifBody,
+		vibrate: []
+	//	icon: notifImg,
 	};
 	new Notification(notifTitle, options);
 	//setTimeout(randomNotification, 3000);
